e8f95c2a77df36cb0692872ff0037ca7db69bfb7
Greyson Newton <greynewt@tamu.edu>
2021-09-23 21:20:07 -0500

    Update strings.java

--- strings.java
+++ strings.java
@@ -1,37 +1,26 @@
-import java.util.Scanner;
-public class strings
-{	public static void main(String[] args)
-	{	
-		bool verified=False
+import java.util.*;
 
-		String rhyme="";
-		System.out.println("\tyeeeeeeeeHAAAA\n------------");// prints out a title,
-	    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("Enter an alter ego:");	// requests a name from the user,
-	    String userName = myObj.nextLine(); //stores
-	    verified=verification(username);	// verifies the name
-	    if verified{
-		    ryhme();		// creates and printsthe verse or rhyme as indicated in the video,
-		    output();	    //thanks user	
-	    }
-	    else{
-	    	//handles wrong name input
-	    	System.out.println("\n------------\n\tu crashedit\n------------");
-	    }
-
+public class NameGame {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("What is your name? ");
+		String name = input.nextLine();
+		
+		int firstSpace = name.indexOf(" ");
+		String firstName = name.substring(0, firstSpace);
+		String lastName = name.substring(firstSpace + 1);	
+		
+		singSong(firstName);
+		System.out.println();
+		singSong(lastName);
 	}
-	public bool verification(String s) //name need for ver.
-	{
-		//codein verification rules
+	
+	public static void singSong(String name) {
+		String suffix = name.substring(1);
+		System.out.println(name + ", " + name + ", bo-b" + suffix);
+		System.out.println("Banana-fana fo-f" + suffix);
+		System.out.println("Fee-fi-fo-m" + suffix);
+		System.out.println(name.toUpperCase() + "!");
 	}
-	public void rhyme() //what input do you need for this?
-	{
-		//codein rhyme rules
-		System.out.println("your rhyme: "+rhyme)
-	}
-	public void output()
-	{
-		System.out.println("Thanksfor your ip ");
-	}
-
 }
